dc7e4baf0fb19e9268ee7c6702fab88716c0569d
Biraj <birajshakya13@gmail.com>
2026-02-09 21:28:52 +0545

    added

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ let habitCount = 0;
 document.addEventListener("DOMContentLoaded", () => {
   //for Vault Count
   function getVault() {
-    for(let i = 0; i < localStorage.length; i++){
+    for (let i = 0; i < localStorage.length; i++) {
       let key = localStorage.key(i);
-      if(key.includes("movie")){
+      if (key.includes("movie")) {
         let data = JSON.parse(localStorage.getItem(key));
         vaultCount++;
         getMovieCard(key, data);
-      } else if(key.includes("habit")){
+      } else if (key.includes("habit")) {
         let data = JSON.parse(localStorage.getItem(key));
         habitCount++;
         getHabitCard(key, data);
-        if(data.keyStatus === "true"){
+        if (data.keyStatus === "true") {
           habitCompleteCount++;
         }
       }
@@ -38,6 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function success(position) {
     let coords = `${position.coords.latitude}, ${position.coords.longitude}`;
+    console.log(position.coords.latitude);
     getWeather(coords);
   }
 
@@ -47,11 +48,11 @@ document.addEventListener("DOMContentLoaded", () => {
   getLocation();
 })
 
-async function getWeather(coords){
-  
+async function getWeather(coords) {
+
   let url = `http://api.weatherapi.com/v1/current.json?key=${weatherApikey}&q=${coords}`;
 
-  try{
+  try {
     let response = await fetch(url);
     let location = await response.json();
 
@@ -59,11 +60,11 @@ async function getWeather(coords){
     let div = document.createElement("div");
     div.id = "weather-card";
     div.classList.add("streak-card");
-    div.innerHTML  = `<div class="habit-card-icon"><i class="fas fa-sun"></i></div>
-                      <div class="streak-card-details">
-                        <p> ${location.current.temp_c}°C ${location.current.condition.text}</p>
-                        <p>${location.location.name}</p>
-                      </div>`;
+    div.innerHTML = `<div class="habit-card-icon"><i class="fas fa-sun"></i></div>
+    <div class="streak-card-details">
+    <p> ${location.current.temp_c}°C ${location.current.condition.text}</p>
+    <p>${location.location.name}</p>
+    </div>`;
     card.appendChild(div);
   } catch(err) {
     console.log(err);
@@ -72,10 +73,12 @@ async function getWeather(coords){
 
 function getProgress(i, j) {
   let card = document.querySelector("#progress-bar div");
-  console.log(card);
-  console.log(i, j);
-  card.style.width = `${i * 100 / j}%`;
-} 
+  if (j >= 1) {
+    card.style.width = `${i * 100 / j}%`;
+  } else {
+    card.style.width = `${j}%`;
+  }
+}
 
 function getVaultCount(j) {
   let card = document.querySelector("#habit-streak-contaienr");
@@ -83,10 +86,10 @@ function getVaultCount(j) {
   div.id = "vault-count-card";
   div.classList.add("streak-card");
   div.innerHTML = `<div class="habit-card-icon"><i class="fas fa-clapperboard"></i></div>
-                  <div class="streak-card-details">
-                    <p>Vault Count</p>
-                    <p>${j} Movie(s)</p>
-                  </div>`;
+  <div class="streak-card-details">
+  <p>Vault Count</p>
+  <p>${j} Movie(s)</p>
+  </div>`;
   card.appendChild(div);
 }
 
@@ -96,10 +99,10 @@ function getHabitCount(j) {
   div.id = "habit-count-card";
   div.classList.add("streak-card");
   div.innerHTML = `<div class="habit-card-icon"><i class="fas fa-dumbbell"></i></div>
-                  <div class="streak-card-details">
-                    <p>Habit Count</p>
-                    <p>${j} Habit(s)</p>
-                  </div>`;
+  <div class="streak-card-details">
+  <p>Habit Count</p>
+  <p>${j} Habit(s)</p>
+  </div>`;
   card.appendChild(div);
 }
 
@@ -109,11 +112,11 @@ function getHabitCard(key, data) {
   div.classList.add("habit-card");
   div.id = `${key}`;
   div.innerHTML = `
-    <div class="habit-card-icon"><i class="fas fa-${data.keyIcon}"></i></div>
-    <div class="habit-card-details">
-      <p>${data.keyName}</p>
-      <p><button class="btn" onclick="habitProgress(this)">${(data.keyStatus === "true")? '<i class="fas fa-check-double"></i>': '<i class="fas fa-check"></i>'}</button></p>
-    </div>
+  <div class="habit-card-icon"><i class="fas fa-${data.keyIcon}"></i></div>
+  <div class="habit-card-details">
+  <p>${data.keyName}</p>
+  <p><button class="btn" onclick="habitProgress(this)">${(data.keyStatus === "true")? '<i class="fas fa-check-double"></i>': '<i class="fas fa-check"></i>'}</button></p>
+  </div>
   `;
   card.prepend(div);
 }
@@ -127,10 +130,10 @@ function getMovieCard(key, data) {
   card.prepend(div);
 }
 
-function habitProgress(btn){
+function habitProgress(btn) {
   let card = btn.parentElement.parentElement.parentElement;
   let data = JSON.parse(localStorage.getItem(card.id));
-  if(data.keyStatus === "false"){
+  if (data.keyStatus === "false") {
     btn.innerHTML = '<i class="fas fa-check-double"></i>';
     data.keyStatus = "true";
     localStorage.setItem(card.id, JSON.stringify(data));
@@ -146,23 +149,51 @@ function habitProgress(btn){
 }
 
 async function searchMovies(input) {
-  if(input.length < 3) return;
-
-  let url = `http://www.omdbapi.com/?apikey=${omdbApliKey}&s=${input}`;
+  if (input.length < 3) return;
   let card = document.querySelector("#search-result");
-  try{
+  
+  card.innerHTML = `<p> Serching for Movies....</p>`;
+  let url = `http://www.omdbapi.com/?apikey=${omdbApliKey}&s=${input}`;
+  try {
     let response = await fetch(url);
     let data = await response.json();
+    let movies = data.Search;
 
     card.innerHTML = "";
 
-    console.log(data);
-  }catch(err){
+    movies.forEach(movie => {
+      let img = new Image();
+      img.src = movie.Poster;
+      img.onload = () => {
+        let div = document.createElement("div");
+        div.classList.add("movie-card");
+        div.id = `movie${movie.imdbId}`;
+        div.style.backgroundImage = `url(${movie.Poster}), url(Images/placeHolder.jpg)`;
+        div.innerHTML = `
+        <div onclick="addMovies(this)" class="icon-container">
+          <i class="fas fa-bookmark"></i>
+        </div>
+        `;
+        card.prepend(div);
+      }
+    })
+  }catch(err) {
     console.error(err);
   }
 }
 
 function debounce(func, delay) {
   let timer;
-  return 
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  }
+}
+
+let searchwithDebounce = debounce(searchMovies, 500)
+
+function addMovies(btn){
+  
 }
